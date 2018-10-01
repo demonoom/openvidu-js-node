7031a5f624bc02bd1f66d4b900a696d482ccfa89
王鹏飞 <398135290@qq.com>
2018-10-01 16:47:35 +0800

    ----

--- openvidu_server.js
+++ openvidu_server.js
@@ -78,10 +78,8 @@ console.log("App listening on port 5000");
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Access-Control-Allow-Headers","Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     next();
 });
 
