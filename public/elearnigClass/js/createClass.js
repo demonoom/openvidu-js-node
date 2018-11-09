d07a8aaa4b083dd301366bb565a79eb07597e243
lidong <1352141077@qq.com>
2018-11-09 15:04:06 +0800

    .

--- public/elearnigClass/js/createClass.js
+++ public/elearnigClass/js/createClass.js
@@ -1,3 +1,33 @@
-$(()=>{
-    console.log(123);
-})
+var service = new Service();
+
+var teacherId = Service.getQueryString("teacherId");
+var teacherName = Service.getQueryString("teacherName");
+
+
+$(document).ready(function(){
+    getUnionClassList();
+});
+
+function getUnionClassList(){
+    var data = {
+        method:"getCurrentUnionClassList",
+        userId:teacherId
+    };
+    service.doWebService(data,{onResponse:function(result){
+        var infos = result.response;
+        console.log(infos,'infos');
+        if(infos.length > 0){
+
+        }else{
+
+        }
+    },onError:function(error){
+
+    }})
+}
+
+function createClass(){
+    console.log(parent.window);
+    parent.window.createClass();
+
+}
