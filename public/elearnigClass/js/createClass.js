2acb6ef047414c345c8133246d15158041f6cb60
luble <282558417@qq.com>
2018-11-14 16:40:29 +0800

    -

--- public/elearnigClass/js/createClass.js
+++ public/elearnigClass/js/createClass.js
@@ -41,7 +41,7 @@ function getUnionClassList() {
                             "    <div class='course-title'><span class='course-titleText public-textHidden'>"+infos[k].courseName+"</span><span class='"+isPublishClass+"'>"+isPublish+"</span></div>" +
                                 "    <div class='course-listText'><span class='course-Light_grey'>授课老师</span><span class='course-Dark_grey'>"+infos[k].users[0].userName+"</span></div>" +
                                 "    <div class='course-listText'><span class='course-Light_grey'>上课时间</span><span class='course-Dark_grey'>"+formatTime(new Date(infos[k].courseTime))+"---"+formatTime(new Date(infos[k].endTime))+"</span></div>" +
-                                "    <div class='course-listText'><span class='course-Light_grey'>课程概述</span><span class='course-Dark_grey public-textHidden course-contText'>"+infos[k].content+"</span></div>" +
+                                "    <div class='course-listText course-listTextLast'><span class='course-Light_grey'>课程概述</span><span class='course-Dark_grey public-textHidden course-contText'>"+infos[k].content+"</span></div>" +
                             "    </div>" +
                             "    <div class='right_oper'>" +
                             "    <div onclick='live_click(\""+infos[k].id+"\","+infos[k].publisher_id+")'><span class='live'></span></div>" +
