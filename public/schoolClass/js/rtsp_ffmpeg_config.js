82ff91964649fb4aa579463f32578da0cbc18e1d
Your Name <you@example.com>
2019-01-03 14:42:48 +0800

    ----

--- public/schoolClass/js/rtsp_ffmpeg_config.js
+++ public/schoolClass/js/rtsp_ffmpeg_config.js
@@ -7,5 +7,5 @@ function RtspFFMPEGConfig(){
 // RtspFFMPEGConfig.zhenlv = "20";
 
 RtspFFMPEGConfig.fenbianlv = "848x480";
-RtspFFMPEGConfig.malv = "1000k";
+RtspFFMPEGConfig.malv = "500k";
 RtspFFMPEGConfig.zhenlv = "20";
