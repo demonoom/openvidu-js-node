da552ee50679bab07e7ffd07e80bf7d84df84187
王鹏飞 <398135290@qq.com>
2019-01-10 20:42:37 +0800

    ---

--- public/schoolClass/js/rtsp_ffmpeg_config.js
+++ public/schoolClass/js/rtsp_ffmpeg_config.js
@@ -6,6 +6,6 @@ function RtspFFMPEGConfig(){
 // RtspFFMPEGConfig.malv = "800k";
 // RtspFFMPEGConfig.zhenlv = "20";
 
-RtspFFMPEGConfig.fenbianlv = "848x480";
+RtspFFMPEGConfig.fenbianlv = "960x720";
 RtspFFMPEGConfig.malv = "800k";
 RtspFFMPEGConfig.zhenlv = "20";
