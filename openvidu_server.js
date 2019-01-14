aa6f57615f189a8f41b06d49cd15079b73e7a4fc
王鹏飞 <398135290@qq.com>
2019-01-14 18:59:40 +0800

    ---

--- openvidu_server.js
+++ openvidu_server.js
@@ -24,7 +24,7 @@ var app = express(); // Create our app with express
 var debug = true;
 
 app.all('*',function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', req.header("Origin"));
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     if (req.method == 'OPTIONS') {
