e3db5e7ad5fb3531d14ea1a5443fdebc3e56f1e2
王鹏飞 <398135290@qq.com>
2019-01-14 17:57:36 +0800

    ---

--- openvidu_server.js
+++ openvidu_server.js
@@ -23,6 +23,18 @@ var bodyParser = require('body-parser'); // Pull information from HTML POST (exp
 var app = express(); // Create our app with express
 var debug = true;
 
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
 // Server configuration
 app.use(session({
     saveUninitialized: true,
